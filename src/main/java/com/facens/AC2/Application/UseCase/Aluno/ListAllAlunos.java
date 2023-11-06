8a2f606b1e2796ab586e9066713a32be3c6c5768
João Ferreira <joaovitor@fasters.com.br>
2023-11-06 14:22:04 -0300

    refactor: remove warnings from ListAllAlunos.java

--- src/main/java/com/facens/AC2/Application/UseCase/Aluno/ListAllAlunos.java
+++ src/main/java/com/facens/AC2/Application/UseCase/Aluno/ListAllAlunos.java
@@ -1,6 +1,5 @@
 package com.facens.AC2.Application.UseCase.Aluno;
 
-import com.facens.AC2.Application.Mapper.Aluno.CreateAlunoMapper;
 import com.facens.AC2.Application.Mapper.Aluno.ListAlunoMapper;
 import com.facens.AC2.Domain.Entity.Aluno;
 import com.facens.AC2.Domain.Repository.AlunoRepository;
@@ -13,15 +12,14 @@ import java.util.List;
 @SpringBootConfiguration
 public class ListAllAlunos {
     private final AlunoRepository alunoRepository;
-    private ListAlunoMapper listAlunoMapper = Mappers.getMapper(ListAlunoMapper.class);
+    private final ListAlunoMapper listAlunoMapper = Mappers.getMapper(ListAlunoMapper.class);
     public ListAllAlunos(AlunoRepository alunoRepository){
         this.alunoRepository = alunoRepository;
     }
 
     public List<ListAlunoDto> list() {
         List<Aluno> aluno = this.alunoRepository.getAll();
-        List<ListAlunoDto> listAlunoDto = listAlunoMapper.alunoToListAlunoDto(aluno);
 
-        return listAlunoDto;
+        return listAlunoMapper.alunoToListAlunoDto(aluno);
     }
 }
