7e7044cf33804ec44aa0cb88669d9169468e1c57
João Ferreira <joaovitor@fasters.com.br>
2023-11-06 14:22:41 -0300

    refactor: remove warnings from AlunoRepository.java

--- src/main/java/com/facens/AC2/Domain/Repository/AlunoRepository.java
+++ src/main/java/com/facens/AC2/Domain/Repository/AlunoRepository.java
@@ -1,14 +1,13 @@
 package com.facens.AC2.Domain.Repository;
 
 import com.facens.AC2.Domain.Entity.Aluno;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface AlunoRepository {
-    public List<Aluno> getAll();
-    public Aluno create(Aluno aluno);
+    List<Aluno> getAll();
+    Aluno create(Aluno aluno);
 
 }
